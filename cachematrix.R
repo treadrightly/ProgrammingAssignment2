e019b143c3024e20580069c5a5746e917b204389
Sampanna Salunke <sampanna.salunke@gmail.com>
2015-01-18 13:27:06 -0800

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# makeCacheMatrix: This function creates a special "matrix" object that can
+# cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) i <<- solve
+  getInverse <- function() i
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+# cacheSolve: This function computes the inverse of the special "matrix" 
+# returned by makeCacheMatrix above. If the inverse has already been calculated
+# (and the matrix has not changed), then the cachesolve should retrieve the 
+# inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
 }
+
+unitTestCase <- function() {
+  x <- matrix(c(4,2,7,6), nrow=2, ncol=2)
+  y <- makeCacheMatrix(x)
+  z <- cacheSolve(y)
+  cat(z)
+  z <- cacheSolve(y)
+  cat(z)
+}
+
+#unitTestCase()
