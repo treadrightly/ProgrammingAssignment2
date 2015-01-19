0d6109647a31c8614451460cabec2511e443a734
Sampanna Salunke <sampanna.salunke@gmail.com>
2015-01-19 13:12:50 -0800

    added check for matrix updates

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,10 @@
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
+    # if set() is called, assume that the original matrix is updated
+    # and invalidate the cache
     x <<- y
+    message("updating original matrix, cache flushed")
     i <<- NULL
   }
   get <- function() x
@@ -16,17 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
 # returned by makeCacheMatrix above. If the inverse has already been calculated
 # (and the matrix has not changed), then the cachesolve should retrieve the 
 # inverse from the cache.
-# input param x: the output of makeCacheMatrix()
-# input param y: the matrix whose inverse is to be computed (ideally, should be same as x$get())
-# the function internally checks if x$get() is identical to y
-# if not identical, then the cache is cleared and the inverse recomputed
-# no error checking is done - e.g., check if x is actually a structure output from
-# makeCacheMatrix etc.
-cacheSolve <- function(x, y, ...) {
-  if(identical(x$get(), y) == FALSE) {
-    message("x$get() and y are different, clearing cache")
-    x$setInverse(NULL)
-  }
+cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   i <- x$getInverse()
   if(!is.null(i)) {
@@ -42,17 +35,18 @@ cacheSolve <- function(x, y, ...) {
 unitTestCase <- function() {
   x <- matrix(c(4,2,7,6), nrow=2, ncol=2)
   y <- makeCacheMatrix(x)
-  z <- cacheSolve(y, x)
+  z <- cacheSolve(y)
   print(z)
-  z <- cacheSolve(y, x)
+  z <- cacheSolve(y)
   print(z)
   m = x %*% z
   print(m)
   y$set(matrix(c(3,2,7,6), nrow=2, ncol=2))
-  z <- cacheSolve(y, x)
+  z <- cacheSolve(y)
   print(z)
   m = y$get() %*% z
   print(m)
 }
 
-#unitTestCase()
+# uncomment test case and source file to run
+# unitTestCase()
